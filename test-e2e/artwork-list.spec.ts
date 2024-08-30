d728fd9fc547423064f14c1fe871c897397ea769
asfalfa <glkin9@gmail.com>
2024-08-30 20:58:36 +0200

    fix(e2e): adjust artwork-list e2e tests

--- test-e2e/artwork-list.spec.ts
+++ test-e2e/artwork-list.spec.ts
@@ -4,17 +4,15 @@ test('renders artwork list', async ({ page }) => {
   await page.goto('/');
   await page.waitForLoadState('networkidle');
 
-  await expect(page.locator('div[role="list"]')).toBeVisible();
+  await expect(page.locator('div').filter({ hasText: 'The BathMary Cassatt1890–' }).nth(2)).toBeVisible();
 });
 
 test('renders artwork cards', async ({ page }) => {
   await page.goto('/');
   await page.waitForLoadState('networkidle');
 
-  const artworkList = page.locator('div[role="list"]');
-
-  await expect(artworkList.locator('div').filter({ hasText: 'The BathMary Cassatt1890–' }).nth(2)).toBeVisible();
-  await expect(artworkList.locator('div').filter({ hasText: 'On a BalconyMary Cassatt1878–' }).nth(2)).toBeVisible();
+  await expect(page.locator('div').filter({ hasText: 'The BathMary Cassatt1890–' }).nth(2)).toBeVisible();
+  await expect(page.locator('div').filter({ hasText: 'On a BalconyMary Cassatt1878–' }).nth(2)).toBeVisible();
 });
 
 test('should load more artworks when scrolling to the bottom', async ({ page }) => {
@@ -25,10 +23,7 @@ test('should load more artworks when scrolling to the bottom', async ({ page }) 
   await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
   await page.evaluate(() => window.scrollTo(0, 0));
   // Expect new artworks to be loaded
-  const newArtwork = page
-    .locator('div[role="list"]')
-    .locator('div')
-    .filter({ hasText: 'GroupGeorge Grey Barnard1903–' })
-    .nth(2);
-  await expect(newArtwork).toBeVisible({ timeout: 10000 });
+  await expect(page.locator('div').filter({ hasText: 'GroupGeorge Grey Barnard1903–' }).nth(2)).toBeVisible({
+    timeout: 10000,
+  });
 });
