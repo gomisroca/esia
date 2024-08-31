9fd3d46d033cd2747816622e0cc651e7f89a43b0
asfalfa <glkin9@gmail.com>
2024-08-31 20:33:04 +0200

    style(config): add Tailwind hoverOnlyWhenSupported

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,9 @@ import { type Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   darkMode: ['class'],
   content: ['./src/**/*.tsx'],
   theme: {
