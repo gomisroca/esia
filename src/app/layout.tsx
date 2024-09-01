c2ecdcd16e4b6ad09f79340f09274fb5edcd2c4f
asfalfa <glkin9@gmail.com>
2024-09-01 22:10:08 +0200

    style(fix): adjust padding bot for root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <div style={{ backgroundImage: "url('/bg.jpg')" }} className="bg-cover bg-fixed bg-center bg-no-repeat">
               <main className="min-h-screen items-center justify-center bg-gradient-to-b from-slate-300/95 via-sky-300/80 to-cyan-300/70 text-white dark:from-slate-900/95 dark:via-sky-900/80 dark:to-cyan-900/70">
                 <Navbar />
-                <div className="container mx-auto flex w-full flex-col items-center justify-center gap-12 px-4">
+                <div className="container mx-auto flex w-full flex-col items-center justify-center gap-12 px-4 pb-8 md:pb-4">
                   {children}
                 </div>
               </main>
