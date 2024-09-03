73d1a6e3e98ef7bd585336bfac766458a1156d30
asfalfa <glkin9@gmail.com>
2024-09-03 12:07:45 +0200

    fix(components): add dropdown prop to close on child click

--- src/app/_components/ui/Dropdown.tsx
+++ src/app/_components/ui/Dropdown.tsx
@@ -3,8 +3,11 @@
 /**
  * Dropdown component that displays a menu when clicked.
  *
+ * @param {{ text: string; className?: string; name?: string }} button - The display in the dropdown button.
+ *
  * @param {string} [className] - Optional class names for the dropdown menu.
  * @param {React.ReactNode} children - The content of the dropdown menu.
+ * @param {boolean} [closeOnChildClick] - Whether to close the dropdown when a child item is clicked.
  *
  * @example
  * <Dropdown name="Select an option" className="w-56" btnClassName="bg-white">
@@ -25,9 +28,10 @@ interface DropdownProps {
   };
   className?: string;
   children: React.ReactNode;
+  closeOnChildClick?: boolean;
 }
 
-function Dropdown({ button, children, className }: DropdownProps) {
+function Dropdown({ button, children, className, closeOnChildClick = true }: DropdownProps) {
   // State variable to track whether the dropdown is open or closed
   const [isOpen, setIsOpen] = useState(false);
 
@@ -69,7 +73,7 @@ function Dropdown({ button, children, className }: DropdownProps) {
           ref={dropdownRef}
           className={`absolute z-50 mt-2 rounded-md border border-neutral-200/30 drop-shadow-md backdrop-blur-sm dark:border-neutral-800/30 ${className ? className : ''}`}>
           <ul
-            onClick={handleChildClick}
+            onClick={closeOnChildClick ? handleChildClick : undefined}
             className="flex flex-wrap items-center justify-center gap-2 rounded-md p-2 text-center font-bold text-neutral-800 transition-all duration-200 dark:text-neutral-200">
             {children}
           </ul>
