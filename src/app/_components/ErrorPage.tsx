3807294b7d99f17e6b77472425e4dad7f44080fc
asfalfa <glkin9@gmail.com>
2024-09-03 12:10:06 +0200

    style(components): adjust spacing of error page

--- src/app/_components/ErrorPage.tsx
+++ src/app/_components/ErrorPage.tsx
@@ -15,9 +15,11 @@ interface ErrorPageProps {
 
 function ErrorPage({ message }: ErrorPageProps) {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 rounded-md bg-neutral-200/40 p-5 text-neutral-800 dark:bg-neutral-800/40 dark:text-neutral-200">
-      <h1>ERROR</h1>
-      <p className="text-xl font-bold">{message}</p>
+    <div className="flex h-[90vh] w-full items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-4 rounded-md bg-neutral-200/40 p-5 text-neutral-800 dark:bg-neutral-800/40 dark:text-neutral-200">
+        <h1 className="text-lg">ERROR</h1>
+        <p className="text-xl font-bold">{message}</p>
+      </div>
     </div>
   );
 }
