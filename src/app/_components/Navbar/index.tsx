ff5f39357e5a0a06ff5318c30af2e52ae084afbf
asfalfa <glkin9@gmail.com>
2024-09-03 08:27:37 +0200

    feat(components): move StyleFilter to navbar

--- src/app/_components/Navbar/index.tsx
+++ src/app/_components/Navbar/index.tsx
@@ -12,12 +12,12 @@ import StyleFilter from './StyleFilter';
 
 async function Navbar() {
   return (
-    <div className="pointer-events-none sticky top-0 z-10 flex h-16 w-full items-center justify-between px-8">
+    <div className="pointer-events-none sticky top-0 z-10 flex h-16 w-full items-center justify-between px-0 md:px-4">
       <BackToTopButton />
-      <div className="pointer-events-auto flex flex-row items-center justify-center gap-4">
-        <StyleFilter />
-        <UserStatus />
+      <div className="pointer-events-auto absolute right-0 top-2 flex flex-col items-center justify-center gap-4 md:right-4 md:top-4">
         <ModeButton />
+        <UserStatus />
+        <StyleFilter />
       </div>
     </div>
   );
