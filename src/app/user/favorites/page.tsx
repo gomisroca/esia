c8f8fc5ecdac48bb9a0dabf3b462a7e9bb65caad
asfalfa <glkin9@gmail.com>
2024-09-05 19:56:05 +0200

    feat(client): add basic user settings and favorite pages

---
+++ src/app/user/favorites/page.tsx
@@ -0,0 +1,30 @@
+import ErrorPage from '@/app/_components/ErrorPage';
+import ArtworkList from '@/app/_components/ui/ArtworkList';
+import { getServerAuthSession } from '@/server/auth';
+import { api } from '@/trpc/server';
+import React from 'react';
+import { type ArtworkWithArtist } from 'types';
+
+async function FavoriteArtworksList() {
+  try {
+    // Get the server-side session,  block page if user is not the logged in user, if user is logged in user, let them rmeove saved artworks from the list
+    const session = await getServerAuthSession();
+    if (!session) return <ErrorPage message="Not logged in" />;
+    // console.log(session);
+
+    const artworkData = await api.users.getFavoriteArtworks();
+    const artworks: ArtworkWithArtist[] = artworkData.map((data) => data.artwork);
+
+    if (!artworks || artworks.length === 0) return <ErrorPage message="No favorite artworks found" />;
+    return (
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-xl font-bold">Favorite Artworks</h1>
+        <ArtworkList artworks={artworks} />
+      </div>
+    );
+  } catch (_error: unknown) {
+    return <ErrorPage message="Something went wrong while fetching your favorite artworks" />;
+  }
+}
+
+export default FavoriteArtworksList;
---
+++ src/app/user/settings/page.tsx
@@ -0,0 +1,39 @@
+import ErrorPage from '@/app/_components/ErrorPage';
+import { getServerAuthSession } from '@/server/auth';
+import { api } from '@/trpc/server';
+import Image from 'next/image';
+import React from 'react';
+
+async function UserSettings() {
+  try {
+    // Get the server-side session, block page if user is not the logged in user, allow setting change otherwise
+    const session = await getServerAuthSession();
+    if (!session) return <ErrorPage message="Not logged in" />;
+
+    const userInfo = await api.users.getInformation();
+    if (!userInfo) return <ErrorPage message="No user found" />;
+    return (
+      <div>
+        <div className="flex flex-row justify-center gap-2">
+          {userInfo.image && (
+            <Image
+              src={userInfo.image}
+              alt={userInfo.name ? userInfo.name : 'User Image'}
+              width={100}
+              height={100}
+              className="rounded-md border-2 border-neutral-200/20 shadow-md dark:border-neutral-800/20"
+            />
+          )}
+          <div className="flex flex-col">
+            <h1 className="text-xl font-bold">{userInfo.name}</h1>
+            <h4>{userInfo.email}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  } catch (_error: unknown) {
+    return <ErrorPage message="Something went wrong while fetching your information" />;
+  }
+}
+
+export default UserSettings;
