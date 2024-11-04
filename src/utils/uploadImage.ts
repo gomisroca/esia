330e5644cd6620f46b73b6916e8e29daf790a674
asfalfa <glkin9@gmail.com>
2024-11-04 12:15:41 +0100

    feat: image upload utils

---
+++ src/utils/uploadImage.ts
@@ -0,0 +1,32 @@
+import supabase from '@/supabase';
+import { v4 as uuidv4 } from 'uuid';
+
+async function getToken(id: string, bucket: string) {
+  const { data } = await supabase.storage.from(bucket).createSignedUploadUrl(`${id}.png`);
+  return data?.token;
+}
+
+async function convertBase64ToFile(dataUrl: string, id: string) {
+  const res: Response = await fetch(dataUrl);
+  const blob: Blob = await res.blob();
+  return new File([blob], id + '.png', { type: 'image/png' });
+}
+
+async function uploadImage(image: string, bucket = 'art') {
+  try {
+    const id = uuidv4();
+    const file = await convertBase64ToFile(image, id);
+
+    const token = await getToken(id, bucket);
+    if (token) {
+      const { data } = await supabase.storage.from(bucket).uploadToSignedUrl(`${id}.png`, token, file);
+      if (data) {
+        return data?.fullPath as string;
+      }
+    }
+  } catch (_error) {
+    throw new Error('Failed to upload image');
+  }
+}
+
+export default uploadImage;
