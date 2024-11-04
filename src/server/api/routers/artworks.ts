bccc2002d77e71cc30983b13803fdcebde3aa190
asfalfa <glkin9@gmail.com>
2024-11-04 12:17:08 +0100

    feat: add artwork routes to create, update, delete and get unique

--- src/server/api/routers/artworks.ts
+++ src/server/api/routers/artworks.ts
@@ -1,8 +1,34 @@
 import { z } from 'zod';
-import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
+import { createTRPCRouter, protectedProcedure, publicProcedure } from '@/server/api/trpc';
 import { TRPCError } from '@trpc/server';
+import uploadImage from '@/utils/uploadImage';
+import { env } from '@/env';
 
 export const artworksRouter = createTRPCRouter({
+  getUnique: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        return ctx.db.artwork.findUnique({
+          where: {
+            id: input.id,
+          },
+          include: {
+            artist: true,
+          },
+        });
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw error;
+        } else {
+          throw new TRPCError({ code: 'NOT_FOUND', message: 'Artwork not found' });
+        }
+      }
+    }),
   getAll: publicProcedure
     .input(
       z.object({
@@ -151,6 +177,154 @@ export const artworksRouter = createTRPCRouter({
           throw error;
         } else {
           throw new TRPCError({ code: 'NOT_FOUND', message: 'Search failed' });
+        }
+      }
+    }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        medium: z.string(),
+        style: z.string(),
+        date: z.string(),
+        origin: z.string(),
+        image: z.string().optional(),
+        artistId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        if (ctx.session.user.admin !== true)
+          throw new TRPCError({ code: 'UNAUTHORIZED', message: 'User not authorized' });
+
+        let imageLink: string | undefined;
+        if (input.image) {
+          try {
+            const imagePath = await uploadImage(input.image);
+            imageLink = env.SUPABASE_PROJECT_URL + '/storage/v1/object/public/' + imagePath;
+          } catch (_error) {
+            throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Failed to upload image' });
+          }
+        }
+
+        const artwork = await ctx.db.artwork.create({
+          data: {
+            name: input.name,
+            medium: input.medium,
+            style: input.style,
+            date: input.date,
+            origin: input.origin,
+            image: imageLink,
+            artistId: input.artistId,
+          },
+        });
+
+        return {
+          id: artwork.id,
+        };
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw error;
+        } else {
+          throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Internal server error' });
+        }
+      }
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        data: z.object({
+          name: z.string(),
+          medium: z.string(),
+          style: z.string(),
+          date: z.string(),
+          origin: z.string(),
+          image: z.string().optional(),
+          artistId: z.string().optional(),
+        }),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        if (ctx.session.user.admin !== true)
+          throw new TRPCError({ code: 'UNAUTHORIZED', message: 'User not authorized' });
+
+        const artwork = await ctx.db.artwork.findUnique({
+          where: {
+            id: input.id,
+          },
+        });
+
+        if (!artwork) {
+          throw new TRPCError({ code: 'NOT_FOUND', message: 'Artwork not found' });
+        }
+
+        let imageLink: string | undefined;
+        if (input.data.image) {
+          try {
+            const imagePath = await uploadImage(input.data.image);
+            imageLink = env.SUPABASE_PROJECT_URL + '/storage/v1/object/public/' + imagePath;
+          } catch (_error) {
+            throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Failed to upload image' });
+          }
+        }
+
+        const data = input.data;
+
+        await ctx.db.artwork.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            name: data.name,
+            medium: data.medium,
+            style: data.style,
+            date: data.date,
+            origin: data.origin,
+            image: imageLink ?? artwork.image,
+            artistId: data.artistId ?? artwork.artistId,
+          },
+        });
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw error;
+        } else {
+          throw new TRPCError({ code: 'NOT_FOUND', message: 'Artwork not found' });
+        }
+      }
+    }),
+  delete: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        if (ctx.session.user.admin !== true)
+          throw new TRPCError({ code: 'UNAUTHORIZED', message: 'User not authorized' });
+
+        const artwork = await ctx.db.artwork.findUnique({
+          where: {
+            id: input.id,
+          },
+        });
+
+        if (!artwork) {
+          throw new TRPCError({ code: 'NOT_FOUND', message: 'Artwork not found' });
+        }
+
+        await ctx.db.artwork.delete({
+          where: {
+            id: input.id,
+          },
+        });
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw error;
+        } else {
+          throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Internal server error' });
         }
       }
     }),
