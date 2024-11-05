7b319ec0370ac4c27fc7225712744c5b965f2115
asfalfa <glkin9@gmail.com>
2024-11-05 14:36:59 +0100

    feat: add blog list and single views

---
+++ src/app/_components/ui/BlogCard.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { type Blog } from '@prisma/client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { memo, useState } from 'react';
+import Title from './Title';
+
+function BlogSkeleton() {
+  return <div data-testid="blog-skeleton" className="h-full w-full animate-pulse rounded-md bg-neutral-800/50" />;
+}
+function BlogCardContent({ blog }: { blog: Blog }) {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <div
+      className="group relative h-[30rem] w-[22rem] cursor-pointer overflow-hidden rounded-md md:h-[45rem] md:w-[30rem]"
+      data-testid="exhibition-card">
+      {/* Skeleton while image is loading */}
+      {!loaded && <BlogSkeleton />}
+
+      {/* Image */}
+      <Image
+        unoptimized
+        onLoad={() => setLoaded(true)}
+        src={blog.headerImage ?? '/ph.jpg'}
+        alt={blog.name}
+        width={300}
+        height={200}
+        className="h-full w-full object-cover transition-transform duration-500 ease-in-out"
+      />
+
+      <Link
+        href={`/blogs/${blog.id}`}
+        className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 transition duration-500 ease-in-out group-hover:scale-105">
+        <div className="min-h-2/3 m-4 w-full rounded-md border-2 border-neutral-200/20 bg-neutral-200/80 p-4 text-center text-neutral-800 drop-shadow-md transition-all duration-500 group-hover:bg-neutral-200 dark:border-neutral-800/20 dark:bg-neutral-800/80 dark:text-neutral-200 group-hover:dark:bg-neutral-800">
+          <Title className="mb-2">{blog.name}</Title>
+          <p className="font-bold">{blog.date.toLocaleDateString()}</p>
+          <div
+            className="overflow-hidden rounded-md bg-slate-900/10 p-2 text-left md:p-4"
+            dangerouslySetInnerHTML={{ __html: blog.content.slice(0, 250) + '...' }}
+          />
+        </div>
+      </Link>
+    </div>
+  );
+}
+const BlogCard = memo((props: Readonly<{ blog: Blog }>) => {
+  return <BlogCardContent {...props} />;
+});
+BlogCard.displayName = 'BlogCard';
+
+export default BlogCard;
---
+++ src/app/blogs/BlogInfiniteList.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { api, type RouterOutputs } from '@/trpc/react';
+import BlogCard from '../_components/ui/BlogCard';
+import { useIntersection } from '@mantine/hooks';
+import { useEffect } from 'react';
+
+type BlogListOutput = RouterOutputs['blogs']['getAll'];
+interface BlogInfiniteListProps {
+  initialBlogs: BlogListOutput;
+}
+export default function BlogInfiniteList({ initialBlogs }: BlogInfiniteListProps) {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = api.blogs.getAll.useInfiniteQuery(
+    {
+      limit: 6,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+      initialData: { pages: [initialBlogs], pageParams: [undefined] },
+    }
+  );
+
+  const { ref, entry } = useIntersection({
+    root: null,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (entry?.isIntersecting && hasNextPage && !isFetchingNextPage) {
+      void fetchNextPage();
+    }
+  }, [entry, fetchNextPage, hasNextPage, isFetchingNextPage]);
+
+  return (
+    <div className="flex flex-col space-y-4">
+      {status === 'pending' ? (
+        <h1 className="p-4 text-xl font-bold">Loading...</h1>
+      ) : status === 'error' ? (
+        <h1 className="p-4 text-xl font-bold">Error fetching blogs</h1>
+      ) : (
+        <>
+          {data?.pages.map((page, i) => (
+            <div className="mx-auto flex w-full flex-wrap items-center justify-center gap-2" role="list" key={i}>
+              {page.blogs.map((blog, index) => (
+                <div key={blog.id} ref={index === page.blogs.length - 1 ? ref : undefined}>
+                  <BlogCard blog={blog} />
+                </div>
+              ))}
+            </div>
+          ))}
+          {isFetchingNextPage && <h1 className="m-auto p-4 text-xl font-bold">Loading more...</h1>}
+        </>
+      )}
+    </div>
+  );
+}
