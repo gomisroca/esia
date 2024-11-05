1508a2a196f945d0e72055b5b1c96a1ab146ba58
asfalfa <glkin9@gmail.com>
2024-11-05 14:35:09 +0100

    feat: add blog creation form

---
+++ src/app/admin/blogs/create/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProtectedRoute from '@/app/_components/ProtectedRoute';
+import BlogCreateForm from './BlogCreateForm';
+
+async function BlogCreate() {
+  return (
+    <ProtectedRoute>
+      <BlogCreateForm />
+    </ProtectedRoute>
+  );
+}
+
+export default BlogCreate;
