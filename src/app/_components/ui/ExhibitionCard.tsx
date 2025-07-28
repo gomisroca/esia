72c01b05fb655f0b5a8c6c01344f9d154d64f971
Adrià Gomis <gomisroca@gmail.com>
2025-07-28 09:58:43 +0200

    style: improve structure of exh card

--- src/app/_components/ui/ExhibitionCard.tsx
+++ src/app/_components/ui/ExhibitionCard.tsx
@@ -9,17 +9,19 @@ import Card from './Card';
 function ExhibitionCardContent({ exhibition }: { exhibition: Exhibition }) {
   return (
     <Card image={exhibition.image ?? undefined} name={exhibition.name}>
-      <Link
-        href={`/exhibitions/${exhibition.id}`}
-        className="absolute inset-0 flex items-center justify-center bg-black/50 transition duration-500 ease-in-out group-hover:scale-105">
-        <div className="m-4 min-h-2/3 w-full rounded-md border-2 border-neutral-200/20 bg-neutral-200/60 p-4 text-center text-neutral-800 drop-shadow-md transition-all duration-500 group-hover:bg-neutral-200/80 dark:border-neutral-800/20 dark:bg-neutral-800/60 dark:text-neutral-200 group-hover:dark:bg-neutral-800/80">
-          <Title className="mb-2">{exhibition.name}</Title>
-          <p>
-            {exhibition.start.toDateString()} - {exhibition.end.toDateString()}
-          </p>
-          <p dangerouslySetInnerHTML={{ __html: exhibition.description }} className="line-clamp-5" />
-        </div>
-      </Link>
+      <section className="flex flex-col gap-4">
+        <Link
+          href={`/exhibitions/${exhibition.id}`}
+          className="flex cursor-pointer transition-transform duration-200 ease-in-out hover:scale-110">
+          <Title>{exhibition.name}</Title>
+        </Link>
+        <p>
+          {exhibition.start.toDateString()} - {exhibition.end.toDateString()}
+        </p>
+      </section>
+      <div className="rounded-md bg-slate-900/10 p-2 text-left md:p-4">
+        <p dangerouslySetInnerHTML={{ __html: exhibition.description }} className="line-clamp-5" />
+      </div>
     </Card>
   );
 }
