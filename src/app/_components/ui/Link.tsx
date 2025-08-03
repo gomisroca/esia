94bff83cd82a83b1f490480d861886084a558810
Adrià Gomis <gomisroca@gmail.com>
2025-08-03 15:05:59 +0200

    feat: use link component in artwork card

--- src/app/_components/ui/Link.tsx
+++ src/app/_components/ui/Link.tsx
@@ -14,7 +14,7 @@ export default function Link({
     <NextLink
       href={href}
       className={twMerge(
-        'm-auto w-fit border-b-2 border-black bg-white/80 px-2 py-1 text-lg font-semibold transition-transform duration-200 ease-in-out hover:scale-110 active:scale-95 dark:border-white dark:bg-black/80',
+        'm-auto w-fit border-b-2 border-black bg-white/80 px-2 py-1 text-lg font-semibold transition-transform duration-200 ease-in hover:scale-110 active:scale-95 dark:border-white dark:bg-black/80',
         className
       )}>
       {children}
