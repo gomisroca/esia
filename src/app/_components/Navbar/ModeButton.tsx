641986bb6ddffeca957c91d4fec363dad1b38e28
Adrià Gomis <gomisroca@gmail.com>
2026-04-24 10:21:36 +0200

    use resolvedTheme since we use default

--- src/app/_components/Navbar/ModeButton.tsx
+++ src/app/_components/Navbar/ModeButton.tsx
@@ -1,24 +1,17 @@
 'use client';
 
-/**
- * Mode button component.
- *
- * @example
- * <ModeButton />
- */
-
 import { useTheme } from 'next-themes';
 import { FaMoon, FaSun } from 'react-icons/fa6';
 
 import Button from '../ui/Button';
 
-function ModeButton() {
-  const { theme, setTheme } = useTheme();
+export default function ModeButton() {
+  const { resolvedTheme, setTheme } = useTheme();
 
   return (
     <Button
       ariaLabel="Theme Button"
-      onClick={() => (theme === 'dark' ? setTheme('light') : setTheme('dark'))}
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
       className="rounded-r-none md:rounded-sm">
       <FaSun className="size-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <FaMoon className="absolute size-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
@@ -26,4 +19,3 @@ function ModeButton() {
     </Button>
   );
 }
-export default ModeButton;
